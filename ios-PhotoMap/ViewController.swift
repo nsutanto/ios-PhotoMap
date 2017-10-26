363148acfd7fe5a094043aab99db14eaf04ae849
nsutanto <nicholas.sutanto@gmail.com>
2017-10-26 18:15:18 -0400

    Init

---
+++ ios-PhotoMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-PhotoMap
+//
+//  Created by Nicholas Sutanto on 10/26/17.
+//  Copyright © 2017 Nicholas Sutanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
