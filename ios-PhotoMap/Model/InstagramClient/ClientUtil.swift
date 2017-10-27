aad2c103cca1c5fc25d40c3d16388bcbb9b3e827
nsutanto <nicholas.sutanto@gmail.com>
2017-10-26 22:25:49 -0400

    refactor client util

---
+++ ios-PhotoMap/Model/InstagramClient/ClientUtil.swift
@@ -0,0 +1,107 @@
+//
+//  ClientUtil.swift
+//  ios-PhotoMap
+//
+//  Created by Nicholas Sutanto on 10/26/17.
+//  Copyright © 2017 Nicholas Sutanto. All rights reserved.
+//
+
+import Foundation
+
+class ClientUtil {
+    
+    var session = URLSession.shared
+    
+    func downloadImage(imageURL: String, completionHandler: @escaping(_ imageData: Data?, _ error: NSError?) ->  Void) -> URLSessionTask {
+        
+        let url = URL(string: imageURL)
+        let request = URLRequest(url: url!)
+        
+        let task = session.dataTask(with: request) {data, response, downloadError in
+            
+            if downloadError != nil {
+                // Do nothing. Task is cancelled.
+            } else {
+                completionHandler(data, nil)
+            }
+        }
+        task.resume()
+        return task
+    }
+    
+    func performRequest(request: NSMutableURLRequest,
+                                completionHandlerRequest: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void)
+        -> URLSessionDataTask {
+            
+            let task = session.dataTask(with: request as URLRequest) { data, response, error in
+                
+                func sendError(_ error: String) {
+                    print(error)
+                    let userInfo = [NSLocalizedDescriptionKey : error]
+                    completionHandlerRequest(nil, NSError(domain: "performRequest", code: 1, userInfo: userInfo))
+                }
+                
+                /* GUARD: Was there an error? */
+                guard (error == nil) else {
+                    sendError("There was an error with your request: \(error!)")
+                    return
+                }
+                
+                /* GUARD: Did we get a successful 2XX response? */
+                guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                    let httpError = (response as? HTTPURLResponse)?.statusCode
+                    sendError("Your request returned a status code : \(String(describing: httpError))")
+                    return
+                }
+                
+                /* GUARD: Was there any data returned? */
+                guard let data = data else {
+                    sendError("No data was returned by the request!")
+                    return
+                }
+                
+                //print(NSString(data: data, encoding: String.Encoding.utf8.rawValue)!)
+                self.convertDataWithCompletionHandler(data, completionHandlerConvertData: completionHandlerRequest)
+            }
+            
+            task.resume()
+            return task
+    }
+    
+    // given raw JSON, return a usable Foundation object
+    func convertDataWithCompletionHandler(_ data: Data, completionHandlerConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject! = nil
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandlerConvertData(parsedResult, nil)
+    }
+    
+    // create a URL from parameters
+    func parseURLFromParameters(_ apiScheme: String,
+                                _ apiHost: String,
+                                _ apiPath: String,
+        _ parameters: [String:AnyObject]?, withPathExtension: String? = nil) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = apiScheme
+        components.host = apiHost
+        components.path = apiPath + (withPathExtension ?? "")
+        components.queryItems = [URLQueryItem]()
+        
+        if let parameters = parameters {
+            for (key, value) in parameters {
+                let queryItem = URLQueryItem(name: key, value: "\(value)")
+                components.queryItems!.append(queryItem)
+            }
+        }
+        
+        print(components.url!.absoluteString)
+        return components.url!
+    }
+}
