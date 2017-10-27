023d6780b7b3a557c95477d252bac33ac11dfb43
nsutanto <nicholas.sutanto@gmail.com>
2017-10-26 23:07:15 -0400

    Clean up instagram constants

--- ios-PhotoMap/Model/InstagramClient/InstagramConstant.swift
+++ ios-PhotoMap/Model/InstagramClient/InstagramConstant.swift
@@ -11,25 +11,28 @@ import Foundation
 extension InstagramClient {
     // MARK: Constants
     struct Constants {
-        
         static let CLIENT_ID  = "2d8f5103db6e40b696fdae1297515b55"
         static let CLIENT_SECRET = "2383a904b3ef41ca9aa476432c617eb9"
+        static let REDIRECT_URI = "https://nsutanto.blogspot.com/"
         
         // MARK: URLs
         static let API_SCHEME = "https"
         static let API_HOST = "api.instagram.com"
-        static let API_PATH = ""
-    
-        //static let INSTAGRAM_AUTHURL = "https://api.instagram.com/oauth/authorize/"
-        //static let INSTAGRAM_APIURl  = "https://api.instagram.com/v1/users/"
         
-        static let REDIRECT_URI = "https://nsutanto.blogspot.com/"
-        static let ACCESS_TOKEN =  "access_token"
         static let INSTAGRAM_SCOPE = "basic"
     }
 
-    // MARK: Methods
-    struct Methods {
+    // MARK: Paths
+    struct Paths {
         static let AUTHORIZE = "/oauth/authorize/"
+    }
+    
+    struct AuthorizationKeys {
+        static let CLIENT_ID  = "client_id"
+        static let REDIRECT_URI = "redirect_uri"
+    }
+    
+    struct AuthorizationResponse {
+        static let ACCESS_TOKEN =  "access_token"
     }
 }
