7f06ab31c3911337a862ebd4d4fb9d2fa4a98f87
nsutanto <nicholas.sutanto@gmail.com>
2017-10-27 22:00:21 -0400

    Change to old web view

--- ios-PhotoMap/ViewController/InstagramLoginViewController.swift
+++ ios-PhotoMap/ViewController/InstagramLoginViewController.swift
@@ -10,61 +10,34 @@ import Foundation
 import UIKit
 import WebKit
 
-extension InstagramLoginViewController: WKNavigationDelegate {
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        print("***** Test TEST")
-        
-        let urlString = webView.url?.absoluteString
-        let accessToken = urlString!.range(of: "#access_token=")
-        print("***** URL String\(String(describing: urlString))")
-        print("***** Access token : \(accessToken)")
-        
-        //print("webView:\(webView) decidePolicyForNavigationAction:\(navigationAction) decisionHandler:\(decisionHandler)")
-        /*
-        switch navigation.navigationType {
-        case .linkActivated: break
-            /*
-             if navigationAction.targetFrame == nil {
-             self.webView?.load(navigationAction.request)
-             }
-             if let url = navigationAction.request.URL,, !url.absoluteString.hasPrefix("http://www.myWebSite.com/example") {
-             UIApplication.sharedApplication().openURL(url)
-             print(url.absoluteString)
-             decisionHandler(.Cancel)
-             return
-             }
-             */
-        default:
-            break
-        }
-        
-        if let url = navigation.request.url {
-            print(url.absoluteString)
-        }
-        decisionHandler(.allow)
- */
-    }
-}
-
-/*
-extension InstagramLoginViewController: WKUIDelegate {
-    /*
+extension InstagramLoginViewController: UIWebViewDelegate {
+    
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        print("***** Test")
+        let requestURLString = (request.url?.absoluteString)! as String
+        print("**** URL String : \(requestURLString)")
+        
+        
+        if requestURLString.hasPrefix(InstagramClient.Constants.REDIRECT_URI) {
+            //let range: Range<String.Index> = requestURLString.range(of: "#access_token=")!
+            let accessToken = requestURLString.range(of: "#access_token=")
+            print("**** ACCESS TOKEN = \(String(describing: accessToken))")
+            //handleAuth(authToken: requestURLString.substring(from: range.upperBound))
+            return false;
+        }
         return true
         //return checkRequestForCallbackURL(request: request)
     }
-    */
+    
 }
-*/
+
 
 class InstagramLoginViewController: UIViewController {
     
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        webView.navigationDelegate = self
+        webView.delegate = self
         
         showInstagramLogin()
     }
@@ -73,11 +46,11 @@ class InstagramLoginViewController: UIViewController {
         
         let url = InstagramClient.sharedInstance().getLoginURL()
         let request = URLRequest(url: url)
-        webView.load(request)
+        webView.loadRequest(request)
     }
     @IBAction func performLogout(_ sender: UIButton) {
         let logoutRequest = URLRequest(url: URL(string: "https://instagram.com/accounts/logout")! as URL)//logout from webview browser
-        webView.load(logoutRequest)
+        webView.loadRequest(logoutRequest)
         //self.dismiss(animated: true, completion: nil)
     }
 }
