dbc402fd806c1248a6b439bd182303001a479fc9
nsutanto <nicholas.sutanto@gmail.com>
2017-10-30 21:51:37 -0400

    Add Coredata stack to app delegate

--- ios-PhotoMap/AppDelegate.swift
+++ ios-PhotoMap/AppDelegate.swift
@@ -12,21 +12,39 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let stack = CoreDataStack(modelName: "Model")!
+    
+    // MARK: Preload Data
+    
+    func preloadData() {
+        
+        // Remove previous stuff (if any)
+        do {
+            try stack.dropAllData()
+        } catch {
+            print("Error droping all objects in DB")
+        }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // preload data from CoreData
+        //preloadData()
+        // Start Autosaving
+        //stack.autoSave(10)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        stack.save()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        stack.save()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
