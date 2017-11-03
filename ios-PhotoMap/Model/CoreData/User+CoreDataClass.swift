2714c962ad7344a42a42bc2b46a92c94613a36de
nsutanto <nicholas.sutanto@gmail.com>
2017-11-03 19:54:47 -0400

    Add basic core data

---
+++ ios-PhotoMap/Model/CoreData/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  ios-PhotoMap
+//
+//  Created by Nicholas Sutanto on 11/3/17.
+//  Copyright © 2017 Nicholas Sutanto. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class User: NSManagedObject {
+
+}
