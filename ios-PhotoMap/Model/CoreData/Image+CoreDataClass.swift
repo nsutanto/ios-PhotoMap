5749863151ed3c3814011b89524bc45fc1cac2ec
nsutanto <nicholas.sutanto@gmail.com>
2017-11-03 22:30:53 -0400

    add image entity

--- ios-PhotoMap/Model/CoreData/Image+CoreDataClass.swift
+++ ios-PhotoMap/Model/CoreData/Image+CoreDataClass.swift
@@ -12,5 +12,21 @@ import CoreData
 
 
 public class Image: NSManagedObject {
-
+    convenience init(id: String, imageURL: String, imageData: NSData?, latitude: Double, longitude: Double, text: String, context: NSManagedObjectContext) {
+        
+        // An EntityDescription is an object that has access to all
+        // the information you provided in the Entity part of the model
+        // you need it to create an instance of this class.
+        if let ent = NSEntityDescription.entity(forEntityName: "Image", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.id = id
+            self.imageURL = imageURL
+            self.imageData = imageData
+            self.latitude = latitude
+            self.longitude = longitude
+            self.text = text
+        } else {
+            fatalError("Unable to find Image entity name!")
+        }
+    }
 }
