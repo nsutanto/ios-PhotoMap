c3d2aec515d0c1a456dfd8192dcd325fd681c4b0
nsutanto <nicholas.sutanto@gmail.com>
2017-11-03 22:30:45 -0400

    add country entity

--- ios-PhotoMap/Model/CoreData/CountryEntity+CoreDataClass.swift
+++ ios-PhotoMap/Model/CoreData/CountryEntity+CoreDataClass.swift
@@ -12,5 +12,16 @@ import CoreData
 
 
 public class CountryEntity: NSManagedObject {
-
+    convenience init(country: String, context: NSManagedObjectContext) {
+        
+        // An EntityDescription is an object that has access to all
+        // the information you provided in the Entity part of the model
+        // you need it to create an instance of this class.
+        if let ent = NSEntityDescription.entity(forEntityName: "CountryEntity", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.country = country
+        } else {
+            fatalError("Unable to find Country entity name!")
+        }
+    }
 }
