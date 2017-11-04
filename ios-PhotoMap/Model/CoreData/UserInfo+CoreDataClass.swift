693e48211a3ca2ddd0ff489ccfa26eee560c8d23
nsutanto <nicholas.sutanto@gmail.com>
2017-11-03 20:22:57 -0400

    Create User Info Managed object.

--- ios-PhotoMap/Model/CoreData/UserInfo+CoreDataClass.swift
+++ ios-PhotoMap/Model/CoreData/UserInfo+CoreDataClass.swift
@@ -12,5 +12,21 @@ import CoreData
 
 
 public class UserInfo: NSManagedObject {
+    
+    convenience init(userName: String, fullName: String, profilePictureURL: String, profilePictureData: NSData?,  context: NSManagedObjectContext) {
+        
+        // An EntityDescription is an object that has access to all
+        // the information you provided in the Entity part of the model
+        // you need it to create an instance of this class.
+        if let ent = NSEntityDescription.entity(forEntityName: "UserInfo", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.userName = userName
+            self.fullName = fullName
+            self.profilePictureURL = profilePictureURL
+            self.profilePictureData = profilePictureData
+        } else {
+            fatalError("Unable to find UserInfo entity name!")
+        }
+    }
 
 }
