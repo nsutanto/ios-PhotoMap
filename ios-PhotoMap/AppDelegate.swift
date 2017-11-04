6d3cb3826a12033eff92c9901f0b189ab2d21de4
nsutanto <nicholas.sutanto@gmail.com>
2017-11-03 21:09:05 -0400

    finish working on model

--- ios-PhotoMap/AppDelegate.swift
+++ ios-PhotoMap/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // preload data from CoreData
-        //preloadData()
+        preloadData()
         // Start Autosaving
         //stack.autoSave(10)
         return true
