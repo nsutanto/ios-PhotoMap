2461692f039021362e7e8a71b8ed8264ebe23ed4
nsutanto <nicholas.sutanto@gmail.com>
2017-11-17 00:59:23 -0500

    Hide / Show login button

--- ios-PhotoMap/ViewController/InitViewController.swift
+++ ios-PhotoMap/ViewController/InitViewController.swift
@@ -12,6 +12,9 @@ import CoreData
 class InitViewController: UIViewController {
     
     var coreDataStack: CoreDataStack?
+    var userToken: String?
+    
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,23 +22,47 @@ class InitViewController: UIViewController {
         let delegate = UIApplication.shared.delegate as! AppDelegate
         coreDataStack = delegate.stack
         
+        // Check if user is already login
+        checkUserInfo()
+        
+        performUIStyle()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if userToken != nil {
+            loginButton.isHidden = true
+        }
+    }
     
+    private func performUIStyle() {
+        loginButton.layer.cornerRadius = 10
+        loginButton.clipsToBounds = true
+    }
+    
+    private func checkUserInfo() {
         // Check if user has the token already. Then no need to show the web login.
         let request: NSFetchRequest<UserInfo> = UserInfo.fetchRequest()
         
         if let result = try? coreDataStack?.context.fetch(request) {
             if let userInfo = result?.first {
                 if userInfo.token != nil {
-                    
-                    let controller = self.storyboard!.instantiateViewController(withIdentifier: "InstagramLoginViewController") as! InstagramLoginViewController
-                    
-                    self.present(controller, animated: true, completion: nil)
+                    userToken = userInfo.token
                 }
             }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    
+        // Check if user has the token already. Then no need to show the web login.
+        if userToken != nil {
+                    
+            let controller = self.storyboard!.instantiateViewController(withIdentifier: "InstagramLoginViewController") as! InstagramLoginViewController
+            
+            self.present(controller, animated: true, completion: nil)
         }
     }
 }
