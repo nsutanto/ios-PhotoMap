628c5f0f8fa1c5da4d90f3c394ef3c6e18d91f1e
nsutanto <nicholas.sutanto@gmail.com>
2017-11-16 21:46:44 -0500

    Skip login if user has the token already

--- ios-PhotoMap/ViewController/InitViewController.swift
+++ ios-PhotoMap/ViewController/InitViewController.swift
@@ -11,27 +11,32 @@ import CoreData
 
 class InitViewController: UIViewController {
     
-    //var coreDataStack: CoreDataStack?
+    var coreDataStack: CoreDataStack?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
+        
         let delegate = UIApplication.shared.delegate as! AppDelegate
         coreDataStack = delegate.stack
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    
+        // Check if user has the token already. Then no need to show the web login.
         let request: NSFetchRequest<UserInfo> = UserInfo.fetchRequest()
         
         if let result = try? coreDataStack?.context.fetch(request) {
             if let userInfo = result?.first {
                 if userInfo.token != nil {
-                    let controller = self.storyboard!.instantiateViewController(withIdentifier: "NavigationController") as! UINavigationController
+                    
+                    let controller = self.storyboard!.instantiateViewController(withIdentifier: "InstagramLoginViewController") as! InstagramLoginViewController
                     
                     self.present(controller, animated: true, completion: nil)
                 }
             }
-            
         }
-        */
     }
 }
 
